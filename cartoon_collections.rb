ef5ce21dfb4ad22b11722a70f5a1f24583a85131
Taylor Swinton <taylorswinton1@gmail.com>
2019-02-14 00:08:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   checkCheese = array.collect do |types| cheese_types.include?(types) end
   if checkCheese.any? {|values| values == true}
-    return "gouda"
+    return 
   else
     return nil
   end
